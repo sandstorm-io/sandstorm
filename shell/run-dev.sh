832cc9d41f3b6d1e2dfb05bb54e40825215b6477
Kenton Varda <temporal@gmail.com>
2014-05-12 19:10:10 -0700

    Update run-dev.sh for oplogs and auth.

--- shell/run-dev.sh
+++ shell/run-dev.sh
@@ -44,8 +44,11 @@ if curl http://localhost:$PORT >/dev/null 2>&1; then
   exit 1
 fi
 
+MONGO_PASSWD=$(</opt/sandstorm/var/mongo/passwd)
+
 export MAIL_URL
-export MONGO_URL="mongodb://127.0.0.1:$MONGO_PORT/meteor"
+export MONGO_URL="mongodb://sandstorm:$MONGO_PASSWD@127.0.0.1:$MONGO_PORT/meteor?authSource=admin"
+export MONGO_OPLOG_URL="mongodb://sandstorm:$MONGO_PASSWD@127.0.0.1:$MONGO_PORT/local?authSource=admin"
 
 SETTINGS=$(mktemp)
 

908c466af3df808ec560db6b341d632945d64a5c
Jason Paryani <github@jparyani.com>
2014-08-13 13:37:26 -0700

    Add login test

--- tests/basic/test-title.js
+++ tests/basic/test-title.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var webdriver = require('selenium-webdriver');
+var webdriver = require('selenium-webdriver'),
+    By        = webdriver.By;
 
 var test = require('selenium-webdriver/testing'),
     describe = test.describe,
@@ -10,25 +11,59 @@ var chai = require('chai'),
     expect = chai.expect,
     assert = chai.assert;
 
-describe('Test sandstorm title', function(){
+describe('Test sandstorm title', function () {
   var driver;
 
-  before(function() {
+  before(function () {
     driver = new webdriver.Builder().
       withCapabilities(webdriver.Capabilities.phantomjs()).
       build();
   });
 
-  describe('Check homepage', function(){
-    it('should see the correct title', function() {
+  describe('Check homepage', function () {
+    it('should see the correct title', function () {
       driver.get('http://localhost:6080');
-      driver.getTitle().then(function(title) {
+      driver.getTitle().then(function (title) {
         expect(title).to.have.string('Sandstorm');
       });
     });
   });
 
-  after(function() {
+  after(function () {
+    driver.quit();
+  });
+});
+
+function waitForElementVisible (driver, selector, timeout) {
+  timeout = timeout || 1000;
+  driver.wait(function () {
+    return driver.findElement(selector).isDisplayed();
+  }, timeout);
+}
+
+describe('Test sandstorm login', function () {
+  var driver;
+
+  before(function () {
+    driver = new webdriver.Builder().
+      withCapabilities(webdriver.Capabilities.phantomjs()).
+      build();
+  });
+
+  describe('Check home page', function () {
+    it('should see allow login as demo user', function() {
+      // Implicitly will wait up to a second for all DOM queries to complete
+      driver.manage().timeouts().implicitlyWait(1000);
+      driver.get('http://localhost:6080/demo');
+
+      driver.findElement(By.id('createDemoUser')).click();
+      driver.findElement(By.id('login-name-link')).getText().then(function (text) {
+        expect(text).to.have.string('Demo User');
+      });
+    });
+  });
+
+  after(function () {
     driver.quit();
   });
 });

427babd5e45a1443c3028f172de84df36740cbae
Jason Paryani <github@jparyani.com>
2014-08-13 22:47:16 -0700

    Remove old selenium-webdriver tests and switch completely to nightwatch

--- tests/nightwatch/test.js
+++ tests/nightwatch/test.js
@@ -11,9 +11,7 @@ module.exports = {
   "Test login" : function (browser) {
     browser
       .url("http://localhost:6080/demo")
-      .waitForElementPresent("#createDemoUser", 1000)
       .click("#createDemoUser")
-      .waitForElementVisible("#login-name-link", 1000)
       .assert.containsText("#login-name-link", "Demo User")
       .saveScreenshot('test.png')
       .end();
--- tests/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nightwatch
-rc=$?
-
-[[ -z "$(jobs -p)" ]] || kill $(jobs -p)
-exit $rc

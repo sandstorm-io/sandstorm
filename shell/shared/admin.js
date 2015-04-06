33ac1556e1a9332e3c16608d0b4f0e80dd267c08
Kenton Varda <kenton@sandstorm.io>
2015-04-06 10:29:13 -0700

    tweak settings page

--- shell/shared/admin.js
+++ shell/shared/admin.js
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var NUM_SETTINGS = 4;
+var NUM_SETTINGS = 3;
 var accountServiceNames = ["google", "github"];
 
 Router.map(function () {

4274a3950bd5d8d712d657ce60e544589281f2f0
Kenton Varda <kenton@sandstorm.io>
2015-06-18 10:56:55 -0700

    Remove bogus line I wrote for debugging that luckily shouldn't have hurt anything.

--- shell/lib/server/migrations.js
+++ shell/lib/server/migrations.js
@@ -75,8 +75,6 @@ var denormalizeInviteInfo = function() {
   });
 }
 
-Meteor.startup(denormalizeInviteInfo);
-
 // This must come after all the functions named within are defined.
 // Only append to this list!  Do not modify or remove list entries;
 // doing so is likely change the meaning and semantics of user databases.

3df1d65726b3ba6f90f4fe7269241ce99d7c8f61
Jason Paryani <github@jparyani.com>
2015-06-29 15:33:56 -0700

    Add ip-networking test

---
+++ tests/apps/ip-networking.js
@@ -0,0 +1,58 @@
+// Sandstorm - Personal Cloud Sandbox
+// Copyright (c) 2014 Sandstorm Development Group, Inc. and contributors
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Here we are testing a toy app (see https://github.com/jparyani/sandstorm-test-app/tree/ip-network
+// for the code). It has a "request" button that tests the basics of an IpNetwork
+
+"use strict";
+
+var utils = require("../utils"),
+    short_wait = utils.short_wait,
+    medium_wait = utils.medium_wait,
+    long_wait = utils.long_wait,
+    very_long_wait = utils.very_long_wait;
+
+module.exports = {};
+
+module.exports["Test Ip Networking"] = function (browser) {
+  browser
+    .init()
+    .loginGithub()
+    .url(browser.launch_url + "/admin/capabilities")
+    .waitForElementVisible("#offer-ipnetwork", short_wait)
+    .click("#offer-ipnetwork")
+    .waitForElementVisible("#powerbox-offer-popup input", short_wait)
+    .getValue("#powerbox-offer-popup input", function(result) {
+      browser
+        .installApp("http://sandstorm.io/apps/jparyani/ip-networking-0.spk", "4829503496b793dffb29040208e35921")
+        .assert.containsText("#grainTitle", "Untitled IpNetworkTest")
+        .pause(short_wait)
+        .frame("grain-frame")
+        .waitForElementVisible("#request", short_wait)
+        .click("#request")
+        .frame()
+        .waitForElementVisible("#powerbox-request-input", short_wait)
+        .setValue("#powerbox-request-input", result.value)
+        .saveScreenshot("test1.png")
+        .click("#powerbox-request-form button")
+        .frame("grain-frame")
+        .pause(1000)
+        .saveScreenshot("test2.png")
+        .waitForElementVisible("#request-result", short_wait)
+        .assert.containsText("#request-result", "301 Moved Permanently");
+    });
+};
+

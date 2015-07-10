e742e922f63035d13f101ce776dbd28da6cdd1b4
Jason Paryani <github@jparyani.com>
2015-07-10 15:51:30 -0700

    Show spinner animation before a grain has responded to its first request
    This has the slight downside of sometimes showing two seperate loading
    screens (1 from this and 1 internal to the app).
    
    Fixes #564

--- shell/server/proxy.js
+++ shell/server/proxy.js
@@ -281,7 +281,8 @@ function openSessionInternal(grainId, user, title, apiToken) {
     grainId: grainId,
     hostId: proxy.hostId,
     timestamp: new Date().getTime(),
-  }
+    hasLoaded: false,
+  };
 
   if (userId) {
     session.userId = userId;
@@ -848,6 +849,7 @@ function Proxy(grainId, ownerId, sessionId, preferredHostId, isOwner, user, user
   this.sessionId = sessionId;
   this.isOwner = isOwner;
   this.isApi = isApi;
+  this.hasLoaded = false;
   if (sessionId) {
     if (!preferredHostId) {
       this.hostId = generateRandomHostname(20);
@@ -1392,6 +1394,15 @@ Proxy.prototype.translateResponse = function (rpcResponse, response) {
     // Add a Content-Security-Policy as a backup in case someone finds a way to load this resource
     // in a browser context. This policy should thoroughly neuter it.
     response.setHeader("Content-Security-Policy", "default-src 'none'; sandbox");
+  }
+
+  // On first response, update the session to have hasLoaded=true
+  if (!this.hasLoaded) {
+    this.hasLoaded = true;
+    var sessionId = this.sessionId;
+    inMeteor(function () {
+      Sessions.update({_id: sessionId}, {$set: {hasLoaded: true}});
+    });
   }
 
   // TODO(security): Set X-Content-Type-Options: nosniff?

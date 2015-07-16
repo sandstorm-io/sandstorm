sandstorm-io/sandstorm

commit de6b63b10b7670fe2deae914195bb1117096c10e

Improvement: grainPermissions() now returns null if the vertex does not have access at all.

shell/server/permissions.js
@@ -194,7 +194,8 @@ function collectEdges(vertex) {
 }
 
 grainPermissions = function(vertex, viewInfo) {
-  // Computes the permissions of a vertex.
+  // Computes the permissions of a vertex. If the vertex is not allowed to open the grain,
+  // returns null. Otherwise, returns an array of bools representing the permissions held.
   var edges = collectEdges(vertex);
   if (edges.openerIsOwner) {
     return roleAssignmentPermissions({allAccess: null}, viewInfo).array;
@@ -205,7 +206,7 @@ grainPermissions = function(vertex, viewInfo) {
     return roleAssignmentPermissions({none: null}, viewInfo).array;
   }
   if (edges.grainDoesNotExist || !edges.terminalEdge || edges.disallowedAnonymousAccess) {
-    return [];
+    return null;
   }
 
   var openerUserId = edges.terminalEdge.sharer;
@@ -216,7 +217,6 @@ grainPermissions = function(vertex, viewInfo) {
   // Keeps track of the permissions that the opener receives from each user. The final result of
   // our computation will be stored in permissionsMap[owner].
 
-  permissionsMap[owner] = new PermissionSet();
   var userStack = [openerUserId];
 
   var openerAttenuation = roleAssignmentPermissions({allAccess: null}, viewInfo);
@@ -249,7 +249,11 @@ grainPermissions = function(vertex, viewInfo) {
       });
     }
   }
-  return permissionsMap[owner].array;
+  if (permissionsMap[owner]) {
+    return permissionsMap[owner].array;
+  } else {
+    return null;
+  }
 }
 
 mayOpenGrain = function(vertex) {

shell/server/core.js
@@ -148,12 +148,12 @@ checkRequirements = function (requirements) {
       }
     } else if (requirement.permissionsHeld) {
       var p = requirement.permissionsHeld;
-      var vertex = {grain: {_id: p.grainId, userId: p.userId}};
-      if (!mayOpenGrain(vertex)) {
+      var viewInfo = Grains.findOne(p.grainId, {fields: {cachedViewInfo: 1}}).cachedViewInfo;
+      var currentPermissions = grainPermissions({grain: {_id: p.grainId, userId: p.userId}},
+                                                viewInfo || {});
+      if (!currentPermissions) {
         return false;
       }
-      var viewInfo = Grains.findOne(p.grainId, {fields: {cachedViewInfo: 1}}).cachedViewInfo;
-      var currentPermissions = grainPermissions(vertex, viewInfo || {});
       for (var ii = 0; ii < p.permissions.length; ++ii) {
         if (p.permissions[ii] && !currentPermissions[ii]) {
           return false;

shell/server/proxy.js
@@ -1082,6 +1082,9 @@ Proxy.prototype._callNewSession = function (request, viewInfo) {
       vertex = {grain: {_id: self.grainId, userId: self.userId}};
     }
     var permissions = grainPermissions(vertex, viewInfo);
+    if (!permissions) {
+      throw new Meteor.Error(403, "Unauthorized", "User is not authorized to open this grain.");
+    }
     Sessions.update({_id: self.sessionId},
                     {$set : {"viewInfo": viewInfo, "permissions": permissions}});
     return permissions;

commit 69378cb2e813e5430aa295c062d72d5350e47992

Fix bug introduced in de6b63b10b7670fe2deae914195bb1117096c10e.

shell/server/permissions.js
@@ -241,7 +241,9 @@ grainPermissions = function(vertex, viewInfo) {
         newPermissions.intersect(permissionsMap[recipient]);
 
         // Optimization: we don't care about permissions that we've already proven the opener has.
-        newPermissions.remove(permissionsMap[owner]);
+        if (permissionsMap[owner]) {
+          newPermissions.remove(permissionsMap[owner]);
+        }
 
         if (permissionsMap[sharer].add(newPermissions)) {
           userStack.push(sharer);
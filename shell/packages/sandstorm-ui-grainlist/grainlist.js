c6ebeaa90107139493b41ee6715570843bf2ec25
Kenton Varda <kenton@sandstorm.io>
2015-08-16 17:14:06 -0700

    Fixes to applist UI (PR #691).
    Functionality issues:
    - Search terms should be conjunctions, not disjunctions. Adding more terms needs to refine the query, not expand it. (Fixed in app list as well.)
    - Added TODO(now) to fix data leak on revoked sharing tokens. Probably need to drop the related functionality, unfortunately.
    
    Style issues:
    - Various readability improvements.
    - Added TODO(cleanup)s describing additional needed fixes.

--- shell/packages/sandstorm-ui-grainlist/grainlist.js
+++ shell/packages/sandstorm-ui-grainlist/grainlist.js
@@ -2,19 +2,30 @@ SandstormGrainList = function (db) {
   this._filter = new ReactiveVar("");
   this._staticHost = db.makeWildcardHost('static');
   var ref = this;
+  // TODO(cleanup): Separate into separate files for client and server. Note that the server side
+  //   can grab the database in method and publish implementations as `this.connection.sandstormDb`,
+  //   so perhaps it's not necessary to construct a SandstormAppList object on the server.
+  // TODO(cleanup): Don't do Meteor.publish, Template.x.{helpers,events}, etc. in constructor code
+  //   since these things cannot be executed multiple times.
   if (Meteor.isServer) {
     // Publish _id, packageId, and lastUsed for grains that you have an API token for.
     Meteor.publish('sharedGrainInfo', function() {
-      // TODO: figure out what to do for not-logged-in users viewing a grain with a sharing link.
-      // For now, return an empty set for non-logged-in users.
+      // Non-logged-in users don't get a "shared with me" list.
+      // TODO(someday): Maybe store a list of tokens in localStorage client-side?
       if (!this.userId) { return []; }
+
+      // TODO(perf): This is possibly really inefficient to create a new subscription for each
+      //   grain. Fortunately we only need it for "shared with me" grains, but if someday users
+      //   have thousansd of grains shared with them this could get really slow. Need something
+      //   better.
       var self = this;
       var grainRefcounts = {};
       var grainSubs = {};
       var refGrain = function (grainId) {
-        if (grainRefcounts[grainId] === undefined) {
+        if (!(grainId in grainRefcounts)) {
           grainRefcounts[grainId] = 0;
-          var thisGrainQuery = db.collections.grains.find({_id: grainId});
+          var thisGrainQuery = db.collections.grains.find(
+              grainId, {fields: {packageId: 1, lastUsed: 1}});
           var thisGrainSub = thisGrainQuery.observe({
             added: function(grain) {
               self.added("grains", grain._id, {
@@ -26,7 +37,8 @@ SandstormGrainList = function (db) {
               self.removed("grains", grain._id);
             },
             updated: function(oldGrain, newGrain) {
-              if (oldGrain.packageId !== newGrain.packageId || oldGrain.lastUsed !== newGrain.lastUsed) {
+              if (oldGrain.packageId !== newGrain.packageId ||
+                  oldGrain.lastUsed !== newGrain.lastUsed) {
                 self.changed("grains", newGrain._id, {
                   packageId: newGrain.packageId,
                   lastUsed: newGrain.lastUsed,
@@ -36,11 +48,10 @@ SandstormGrainList = function (db) {
           });
           grainSubs[grainId] = thisGrainSub;
         }
-        grainRefcounts[grainId] = grainRefcounts[grainId] + 1;
+        ++grainRefcounts[grainId];
       };
       var unrefGrain = function (grainId) {
-        grainRefcounts[grainId] = grainRefcounts[grainId] - 1;
-        if (grainRefcounts[grainId] === 0) {
+        if (--grainRefcounts[grainId] === 0) {
           delete grainRefcounts[grainId];
           var sub = grainSubs[grainId];
           delete grainSubs[grainId];
@@ -48,6 +59,11 @@ SandstormGrainList = function (db) {
         }
       };
 
+      // TODO(now): TODO(security): This reveals information (package ID, last activity time, and
+      //   existence) of REVOKED tokens. Computing whether or not tokens have been revoked is
+      //   probably too inefficient to do here. In fact, revealing package ID even to non-revoked
+      //   sharees is arguably wrong since the package could be private, and having the package ID
+      //   is sufficient to install the app.
       var apiTokens = db.collections.apiTokens.find({'owner.user.userId': this.userId});
       var apiTokensHandle = apiTokens.observe({
         added: function(newToken) {
@@ -83,18 +99,16 @@ SandstormGrainList = function (db) {
       // split up searchString into an array of regexes, use them to match against item
       var searchKeys = searchString.toLowerCase()
           .split(" ")
-          .filter(function(k) { return k != "";});
+          .filter(function(k) { return k !== "";});
       return function matchFilter(item) {
-        // special case: no text in filter should pass all items
-        if (searchKeys.length === 0) return true;
-        // Keep any item that matches any given substring
+        // Keep any item that matches all substrings
         var haystack = item.title.toLowerCase();
         for (var i = 0 ; i < searchKeys.length ; i++) {
-          if (haystack.indexOf(searchKeys[i]) !== -1) {
-            return true;
+          if (haystack.indexOf(searchKeys[i]) === -1) {
+            return false;
           }
         }
-        return false;
+        return true;
       };
     };
     var mapGrainsToTemplateObject = function (grains) {
@@ -107,8 +121,9 @@ SandstormGrainList = function (db) {
       var packagesById = _.indexBy(packages, '_id');
       return grains.map(function(grain) {
         var pkg = packagesById[grain.packageId];
-        var iconSrc = pkg ? iconSrcForPackage(pkg, 'grain', ref._staticHost) : "";
-        var appTitle = (pkg && pkg.manifest && pkg.manifest.appTitle && pkg.manifest.appTitle.defaultText) || "";
+        var iconSrc = pkg ? Identicon.iconSrcForPackage(pkg, 'grain', ref._staticHost) : "";
+        var appTitle = (pkg && pkg.manifest && pkg.manifest.appTitle &&
+                        pkg.manifest.appTitle.defaultText) || "";
         return {
           _id: grain._id,
           title: grain.title,
@@ -131,8 +146,9 @@ SandstormGrainList = function (db) {
       var packagesById = _.indexBy(packages, '_id');
       return sharedGrains.map(function(grain) {
         var pkg = packagesById[grain.packageId];
-        var iconSrc = pkg ? iconSrcForPackage(pkg, 'grain', ref._staticHost) : "";
-        var appTitle = (pkg && pkg.manifest && pkg.manifest.appTitle && pkg.manifest.appTitle.defaultText) || "";
+        var iconSrc = pkg ? Identicon.iconSrcForPackage(pkg, 'grain', ref._staticHost) : "";
+        var appTitle = (pkg && pkg.manifest && pkg.manifest.appTitle &&
+                        pkg.manifest.appTitle.defaultText) || "";
         // It's theoretically possible to have multiple API tokens for the same grain.
         // Pick one arbitrarily to assign the grain petname from.
         var token = tokensForGrain[grain._id][0];
@@ -180,10 +196,10 @@ SandstormGrainList = function (db) {
         ref._filter.set(event.target.value);
       },
       "keypress .search-bar": function(event) {
-        if (event.keyCode == 13) {
+        if (event.keyCode === 13) {
           // Enter pressed.  If a single grain is shown, open it.
           var grains = filteredSortedGrains();
-          if (grains.length == 1) {
+          if (grains.length === 1) {
             // Unique grain found with current filter.  Activate it!
             var grainId = grains[0]._id;
             // router.go grain/grainId?

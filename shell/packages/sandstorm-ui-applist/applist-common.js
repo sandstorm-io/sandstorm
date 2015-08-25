e3cfde026a8c40ba128fd0a65ce76668b3b01977
Drew Fisher <drew@sandstorm.io>
2015-08-25 11:17:01 -0700

    Sort apps alphabetically, not reverse-alphabetically.
    Apps with inferred app titles are sorted first, since null sorts first.

--- shell/packages/sandstorm-ui-applist/applist-common.js
+++ shell/packages/sandstorm-ui-applist/applist-common.js
@@ -1,7 +1,7 @@
 // TODO(cleanup): Make this class client-only since it does nothing useful on the server.
 SandstormAppList = function(db, quotaEnforcer) {
   this._filter = new ReactiveVar("");
-  this._sortOrder = new ReactiveVar([["appTitle", "desc"]]);
+  this._sortOrder = new ReactiveVar([["appTitle", 1]]);
   this._staticHost = db.makeWildcardHost("static");
   this._db = db;
   this._quotaEnforcer = quotaEnforcer;

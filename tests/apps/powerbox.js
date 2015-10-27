6c89a756ebffe768916e1f809b0453eafe63fb70
Jason Paryani <github@jparyani.com>
2015-10-26 18:11:26 -0700

    Add test for powerbox membranes

--- tests/apps/powerbox.js
+++ tests/apps/powerbox.js
@@ -132,3 +132,80 @@ module.exports["Test Powerbox with failing requirements"] = function (browser) {
           });
     });
 };
+
+// This test revokes a permission of a live powerbox cap, and expects an error to occur.
+// Source at https://github.com/jparyani/sandstorm-test-app/tree/powerbox-membrane
+module.exports["Test Powerbox membrane"] = function (browser) {
+  var firstUserName;
+  var secondUserName;
+  var grainUrl;
+  browser
+    .init()
+    .installApp("http://sandstorm.io/apps/jparyani/powerbox-membrane-0.spk", "7a555b1fc63d7fbb07109c8cf8fd9ed3", "10p6jj3zzkh5v9ymmcmjaj8cj2yqx29j1g1vku7nt71pt1c28cmh")
+    .assert.containsText("#grainTitle", "Untitled PowerboxMembraneTest")
+    .execute(function () { return Meteor.user().identities[0].service.dev.name; }, [], function(result) {
+      firstUserName = result.value;
+    })
+    .url(function (url) {
+      grainUrl = url.value;
+    })
+    .click('.topbar .share > .show-popup')
+    .waitForElementVisible("#shareable-link-tab-header", short_wait)
+    .click("#shareable-link-tab-header")
+    .waitForElementVisible(".new-share-token", short_wait)
+    .submitForm('.new-share-token')
+    .waitForElementVisible('#share-token-text', medium_wait)
+    // Navigate to the url with 2nd user
+    .getText('#share-token-text', function(response) {
+      browser
+        .loginDevAccount()
+        .execute(function () { return Meteor.user().identities[0].service.dev.name; }, [], function(result) {
+          secondUserName = result.value;
+        })
+        .url(response.value)
+        .waitForElementVisible(".redeem-token-button", short_wait)
+        .click(".redeem-token-button")
+        .waitForElementVisible('#grainTitle', medium_wait)
+        .pause(short_wait)
+        .frame("grain-frame")
+        .waitForElementVisible("#offer", short_wait)
+        .click("#offer")
+        .waitForElementVisible("#offer-result", short_wait)
+        .assert.containsText("#offer-result", "offer: success")
+        .frame()
+        .waitForElementVisible("#powerbox-offer-url", short_wait)
+        .getText("#powerbox-offer-url", function (result) {
+            browser
+              .frame("grain-frame")
+              .click("#request")
+              .frame()
+              .waitForElementVisible("#powerbox-request-input", short_wait)
+              .setValue("#powerbox-request-input", result.value)
+              .click("#powerbox-request-form button")
+              .frame("grain-frame")
+              .waitForElementVisible("#request-result", short_wait)
+              .assert.containsText("#request-result", "request: footest")
+              .click("#test")
+              .waitForElementVisible("#testInterface-result", short_wait)
+              .assert.containsText("#testInterface-result", "testInterface: footest")
+              .loginDevAccount(firstUserName)
+              .url(grainUrl)
+              .waitForElementVisible("#grainTitle", short_wait)
+              .click('.topbar .share > .show-popup')
+              .waitForElementVisible(".who-has-access", short_wait)
+              .click(".who-has-access")
+              .waitForElementVisible('.popup.who-has-access', short_wait)
+              .waitForElementVisible(".popup.who-has-access .share-token-role", short_wait)
+              .pause(short_wait)
+              .setValue(".popup.who-has-access .share-token-role", "can read")
+              .pause(short_wait)
+              .loginDevAccount(secondUserName)
+              .url(grainUrl)
+              .pause(short_wait)
+              .frame('grain-frame')
+              .click("#test")
+              .waitForElementVisible("#testInterface-result", short_wait)
+              .assert.containsText("#testInterface-result", "testInterface: couldn't call testInterface");
+        });
+    });
+};

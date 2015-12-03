2ff07ff70648a748264e57209ddee905d0e9a4b4
Jason Paryani <github@jparyani.com>
2015-12-03 14:00:06 -0800

    Make UserContacts fully reactive

--- shell/packages/sandstorm-ui-autocomplete-input/autocomplete-server.js
+++ shell/packages/sandstorm-ui-autocomplete-input/autocomplete-server.js
@@ -2,15 +2,50 @@ Meteor.publish("userContacts", function () {
   // TODO(someday): make this reactive
   var db = this.connection.sandstormDb;
   var self = this;
-  var contacts = db.collections.contacts.find({ownerId: this.userId});
-  var identityIds = _.pluck(contacts.fetch(), "identityId");
-  var identities = Meteor.users.find({_id: {$in: identityIds}});
 
-  identities.forEach(function (identity) {
-    SandstormDb.fillInProfileDefaults(identity);
-    SandstormDb.fillInIntrinsicName(identity);
-    self.added("userContacts", identity._id,  _.pick(identity, ["_id", "profile"]));
+  // We maintain a map from identity IDs to live query handles that track profile changes.
+  var loginIdentities = {};
+
+  var self = this;
+  function addIdentityOfContact(contact) {
+    if (!(contact.identityId in loginIdentities)) {
+      var user = Meteor.users.findOne({_id: contact.identityId});
+      if (user) {
+        SandstormDb.fillInProfileDefaults(user);
+        SandstormDb.fillInIntrinsicName(user);
+        var filteredUser = _.pick(user, "_id", "profile");
+        self.added("userContacts", user._id, filteredUser);
+      }
+      loginIdentities[contact.identityId] =
+        Meteor.users.find({_id: contact.identityId}, {fields: {profile: 1}}).observeChanges({
+          changed: function (id, fields) {
+            self.changed("userContacts", id, fields);
+          }
+        });
+    }
+  }
+  var cursor = db.collections.contacts.find({ownerId: this.userId});
+
+  var handle = cursor.observe({
+    added: function (contact) {
+      addIdentityOfContact(contact);
+    },
+    changed: function (contact) {
+      addIdentityOfContact(contact);
+    },
+    removed: function (contact) {
+      self.removed("userContacts", contact.identityId);
+      loginIdentities[contact.identityId].stop();
+      delete loginIdentities[contact.identityId];
+    },
   });
-
   this.ready();
+
+  this.onStop(function() {
+    handle.stop();
+    Object.keys(loginIdentities).forEach(function(identityId) {
+      loginIdentities[identityId].stop();
+      delete loginIdentities[identityId];
+    });
+  });
 });

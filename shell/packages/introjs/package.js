837e718e9b4a9ce01c141923183ab643ff50d7ed
Asheesh Laroia <asheesh@sandstorm.io>
2016-01-08 13:16:50 -0800

    Move introjs into a package

---
+++ shell/packages/introjs/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'introjs',
+  version: '2.0.0',
+  summary: 'A copy of introjs, packaged as a Meteor package',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('client/intro.js', 'client');
+  api.addFiles('client/introjs.css', 'client');
+});

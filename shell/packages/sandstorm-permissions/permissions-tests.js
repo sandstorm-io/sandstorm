d5ac153eb822da1b387a2e2735210c6022d778fb
Drew Fisher <drew@sandstorm.io>
2016-02-14 02:31:27 -0800

    JSCS: ignore sandstorm-permissions
    I'm leaving sandstorm-permissions basically untouched so @dwrensha doesn't have
    to do a massive invasive rebase while implementing the new permissions
    computation.
    
    With this change, we can now run JSCS on all of shell/ and pass.

--- shell/packages/sandstorm-permissions/permissions-tests.js
+++ shell/packages/sandstorm-permissions/permissions-tests.js
@@ -14,6 +14,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Until David is done working on this:
+// jscs:disable
+
 var Crypto = Npm.require("crypto");
 
 var globalDb = new SandstormDb();

2ab14d1873358fdcdad1d1b6c404028f8c3d09f4
Kenton Varda <kenton@sandstorm.io>
2016-02-18 19:25:08 -0800

    Fix seccomp breakage.

---
+++ src/libseccomp/include/seccomp-in.h
@@ -0,0 +1,1 @@
+../../../deps/libseccomp/include/seccomp.h.in
--- src/libseccomp/include/seccomp.h
+++ src/libseccomp/include/seccomp.h
@@ -1,1 +1,12 @@
-../../../deps/libseccomp/include/seccomp.h.in
+// Horrible hack for Sandstorm. See: ../README
+
+#include "seccomp-in.h"
+
+#undef SCMP_VER_MAJOR
+#undef SCMP_VER_MINOR
+#undef SCMP_VER_MICRO
+
+#define SCMP_VER_MAJOR 0
+#define SCMP_VER_MINOR 0
+#define SCMP_VER_MICRO 0
+

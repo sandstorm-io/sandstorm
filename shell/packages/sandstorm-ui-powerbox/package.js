538de280306004ca5c8546f459323364da04109a
Kenton Varda <kenton@sandstorm.io>
2016-03-18 17:20:38 -0700

    Refactor Picker Powerbox so that query matching happens on the server side.
    PowerboxDescriptors are now provided in base64url-serialized-packed Cap'n Proto format, not JSON.
    
    This will be necessary going forward because PowerboxDescriptor.Tag.value is an AnyPointer, the schema of which Sandstorm may not even know. The query-matching algorithm is designed to operate on Cap'n Proto binary format without the need for a schema, but without a schema it is not possible to convert to/from JSON.
    
    I expect most apps won't need to customize their queries very much, so can probably get away with using hard-coded queries copied from documentation. Copying a whole query string seems not much worse than copying a type ID.

--- shell/packages/sandstorm-ui-powerbox/package.js
+++ shell/packages/sandstorm-ui-powerbox/package.js
@@ -20,7 +20,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(["ecmascript", "check", "reactive-var", "templating", "tracker", "underscore", "sandstorm-db", "sandstorm-ui-topbar"], "client");
+  api.use(["ecmascript", "check", "reactive-var", "templating", "tracker", "underscore", "sandstorm-db", "sandstorm-ui-topbar", "mongo"], "client");
   api.use(["ecmascript", "check"], "server");
   api.addFiles(["powerbox.html", "powerbox-client.js"], "client");
   api.addFiles(["powerbox-server.js"], "server");

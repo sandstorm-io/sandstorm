8235b99b60b6bc240b3d32471843cc9431237013
Kenton Varda <kenton@sandstorm.io>
2016-03-20 00:10:40 -0700

    Attempt to fix bug reported at https://twitter.com/fink_/status/709715523513270272

--- shell/packages/accounts-email-token/token_common.js
+++ shell/packages/accounts-email-token/token_common.js
@@ -22,6 +22,20 @@ Router.route("/_emailLogin/:_email/:_token", function () {
       Router.go("/");
     }
   });
+}, {
+  // Specify a name in an attempt to fix bug reported here:
+  //   https://twitter.com/fink_/status/709715523513270272
+  //
+  // The user reported an exception trace where IronRouter complained that the route "i" had been
+  // registered twice, tracing back to routes in this file. Where did "i" come from? The problem
+  // does not seem to be reproducible.
+  //
+  // It looks like IronRouter may decide to name the route based on the function name:
+  //   https://github.com/iron-meteor/iron-middleware-stack/blob/master/lib/handler.js#L49
+  //
+  // Our function has no name, but perhaps minification can mess with that? An explicit name
+  // in the options will take precedence, so do that.
+  name: "_emailLogin",
 });
 
 Router.route("/_emailLinkIdentity/:_email/:_token/:_accountId", function () {
@@ -42,4 +56,7 @@ Router.route("/_emailLinkIdentity/:_email/:_token/:_accountId", function () {
       this.render("_emailLinkIdentityError");
     }
   }
+}, {
+  // See above.
+  name: "_emailLinkIdentity",
 });

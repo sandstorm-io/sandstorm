062323fb5c539ad257175d443d1fcc020d3bc7a9
Kenton Varda <kenton@sandstorm.io>
2016-08-29 12:30:36 -0700

    Remove QtCreator work-around for KJ_MVCAP -- not needed with Clang code model.

--- src/sandstorm/util.h
+++ src/sandstorm/util.h
@@ -39,15 +39,10 @@ namespace kj {
 
 namespace sandstorm {
 
-#if __QTCREATOR
-#define KJ_MVCAP(var) var
-// QtCreator dosen't understand C++14 syntax yet.
-#else
 #define KJ_MVCAP(var) var = ::kj::mv(var)
 // Capture the given variable by move.  Place this in a lambda capture list.  Requires C++14.
 //
 // TODO(cleanup):  Move to libkj.
-#endif
 
 typedef unsigned int uint;
 typedef unsigned char byte;

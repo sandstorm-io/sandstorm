5bf5b2a5acc7131c5bb328f9e53c15761c20efa6
Kenton Varda <kenton@sandstorm.io>
2016-09-22 17:27:54 -0700

    Fix that the preinstalled app package IDs could become outdated.
    When an app update is discovered through an app index refresh, if the newer version of the app was already installed (for example, because someone had gone to the app market and installed it manually before the index update landed), then the preinstall package ID was never updated. This means the old version continued to be preinstalled and also broke appdemos of the particular app.

--- shell/packages/sandstorm-db/db.js
+++ shell/packages/sandstorm-db/db.js
@@ -1694,6 +1694,10 @@ _.extend(SandstormDb.prototype, {
         } else {
           this.startInstall(app.packageId, url, false, true);
         }
+      } else if (isAppPreinstalled) {
+        // The new version is already installed, but we still need to update the preinstall
+        // packageId.
+        globalDb.setPreinstallAppAsReady(app.appId, app.packageId);
       }
     });
   },

4143ed22aaf439dfd3ffa4894abb1feeae3312a6
Kenton Varda <kenton@sandstorm.io>
2016-11-15 01:36:31 -0800

    Fix IE.

--- shell/imports/client/standalone.js
+++ shell/imports/client/standalone.js
@@ -4,7 +4,9 @@ parser.href = __meteor_runtime_config__.ROOT_URL;
 // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
 
 const isStandalone = function () {
-  return window.location.host !== parser.host;
+  // Note: Don't compare by `host` because IE11 incorrectly adds ":443" to parser.host if it's an
+  //   HTTPS URL. :(
+  return window.location.hostname !== parser.hostname;
 };
 
 export { isStandalone };

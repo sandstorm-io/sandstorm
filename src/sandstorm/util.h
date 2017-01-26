9a4d45322e020bde72d268665e53bcff48c353d3
Kenton Varda <kenton@sandstorm.io>
2017-01-26 01:10:54 -0800

    Add OwnCapnp util class which is a pass-by-move capnp value.

--- src/sandstorm/util.h
+++ src/sandstorm/util.h
@@ -495,6 +495,38 @@ private:
   void taskFailed(kj::Exception&& exception) override;
 };
 
+template <typename T>
+class OwnCapnp;
+
+template <typename Reader>
+OwnCapnp<capnp::FromReader<Reader>> newOwnCapnp(Reader value);
+
+template <typename T>
+class OwnCapnp: public T::Reader {
+  // A copy of a capnp object which lives in-memory and can be passed by ownership.
+
+public:
+  // Inherits methods of reader.
+
+private:
+  kj::Array<capnp::word> words;
+
+  OwnCapnp(kj::Array<capnp::word> words)
+      : T::Reader(capnp::readMessageUnchecked<T>(words.begin())),
+        words(kj::mv(words)) {}
+
+  template <typename Reader>
+  friend OwnCapnp<capnp::FromReader<Reader>> newOwnCapnp(Reader value);
+};
+
+template <typename Reader>
+OwnCapnp<capnp::FromReader<Reader>> newOwnCapnp(Reader value) {
+  auto words = kj::heapArray<capnp::word>(value.totalSize().wordCount + 1);
+  memset(words.asBytes().begin(), 0, words.asBytes().size());
+  capnp::copyToUnchecked(value, words);
+  return OwnCapnp<capnp::FromReader<Reader>>(kj::mv(words));
+}
+
 }  // namespace sandstorm
 
 #endif // SANDSTORM_UTIL_H_

5c22256f75f7a76e45eacdb36e23a136aa1df19c
Ian Denhardt <ian@zenhack.net>
2020-01-29 00:46:30 -0500

    Delete some commented-out code.

--- meteor-testapp/client/main.js
+++ meteor-testapp/client/main.js
@@ -1,12 +1,6 @@
 import { Template } from 'meteor/templating';
-// import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   // this.counter = new ReactiveVar(0);
-// });
 
 Template.hello.helpers({
   id() {
@@ -25,10 +19,3 @@ Template.hello.helpers({
     return Meteor.sandstormUser().pronouns;
   },
 });
-
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },xz
-// });

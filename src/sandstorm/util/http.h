2a4f59481325149d97a10009f1a7d5dd0f4eceec
Ian Denhardt <ian@zenhack.net>
2021-06-12 13:08:57 -0400

    Formatting
    Co-authored-by: Kenton Varda <kenton@sandstorm.io>

--- src/sandstorm/util/http.h
+++ src/sandstorm/util/http.h
@@ -4,21 +4,21 @@
 namespace sandstorm::util::http {
 
 class ExtraHeadersResponse final : public kj::HttpService::Response {
-  public:
-    ExtraHeadersResponse(kj::HttpService::Response& orig, kj::HttpHeaders&& extraHeaders);
+public:
+  ExtraHeadersResponse(kj::HttpService::Response& orig, kj::HttpHeaders&& extraHeaders);
 
-    kj::HttpHeaders& headers();
+  kj::HttpHeaders& headers();
 
-    // Implements Response
-    kj::Own<kj::AsyncOutputStream> send(
-        kj::uint statusCode, kj::StringPtr statusText, const kj::HttpHeaders& headers,
-        kj::Maybe<uint64_t> expectedBodySize = nullptr);
-    kj::Own<kj::WebSocket> acceptWebSocket(const kj::HttpHeaders& headers);
+  // Implements Response
+  kj::Own<kj::AsyncOutputStream> send(
+      kj::uint statusCode, kj::StringPtr statusText, const kj::HttpHeaders& headers,
+      kj::Maybe<uint64_t> expectedBodySize = nullptr);
+  kj::Own<kj::WebSocket> acceptWebSocket(const kj::HttpHeaders& headers);
 
-  private:
-    void updateHeaders(const kj::HttpHeaders& headers);
+private:
+  void updateHeaders(const kj::HttpHeaders& headers);
 
-    kj::HttpService::Response& origResponse;
-    kj::HttpHeaders extraHeaders;
+  kj::HttpService::Response& origResponse;
+  kj::HttpHeaders extraHeaders;
 };
 };

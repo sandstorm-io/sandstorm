5397861a98ee592ebd922fdc1fa2ef381953f6ca
Eric Schultz <eric@commitchange.com>
2024-02-18 12:46:01 -0600

    Configure container build

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM docker.io/ubuntu:20.04
+WORKDIR /sandstorm
+RUN apt-get update &&\
+    DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential libcap-dev xz-utils zip \
+    unzip strace curl discount git python3 zlib1g-dev \
+    cmake flex bison locales clang gcc-multilib
+RUN curl -L "https://go.dev/dl/go1.21.6.linux-amd64.tar.gz" -o go.tar.gz  \
+    && tar -C /usr/local -xvf go.tar.gz \
+    && rm go.tar.gz
+RUN curl -L "https://nodejs.org/dist/v10.24.1/node-v10.24.1-linux-x64.tar.gz" -o node.tar.gz \
+    && tar -C /usr/local -xvf node.tar.gz \
+    && rm node.tar.gz
+RUN curl https://install.meteor.com/ | sh
+ENV PATH "$PATH:/usr/local/go/bin:/usr/local/node/bin"
+RUN chown -R root:root /usr/local/node-v10.24.1-linux-x64
+RUN ln -s /usr/local/node-v10.24.1-linux-x64 /usr/local/node
+CMD  "/bin/sh"
---
+++ container-build.py
@@ -0,0 +1,43 @@
+#!/bin/python
+import argparse, shlex, subprocess, os
+
+# from https://stackoverflow.com/a/4760274/259046
+def runProcess(exe):
+    p = subprocess.run(shlex.split(exe), stderr=subprocess.STDOUT)
+    while(True):
+        if not isinstance(p, subprocess.CompletedProcess):
+            retcode = p.poll()
+        else:
+            retcode = p.returncode
+        
+        stdout = p.stdout
+        print(stdout)
+        if retcode is not None:
+            return retcode
+
+
+
+
+parser = argparse.ArgumentParser(description='Build Sandstorm using an Ubuntu 20.04 Docker/OCI container')
+parser.add_argument("action", choices=["make", "prepare"], default="make", nargs="?")
+parser.add_argument('--container-builder', dest="container_builder", default='podman', help='Command you run for building container from command line')
+parser.add_argument('--container-runner', dest="container_runner", default='podman', help='Command you run for running container from command line')
+
+args = parser.parse_args()
+
+def prepare():
+    runProcess(args.container_builder + ' build . -t sandstorm-build')
+
+def make():
+    runProcess(args.container_runner + ' run --rm -ti -v ' + os.getcwd() + ':/sandstorm -u ' +  str(os.getuid()) + ' --cap-add=SYS_PTRACE --env \'USER\' sandstorm-build make')
+
+
+prepare()
+
+if (args.action == "make"):
+    make()
+
+
+
+
+

5c2f44e4a0b7b1004c35be0e559bac00232204c1
Eric Schultz <eric@wwahammy.com>
2024-10-06 10:21:54 -0500

    Fix unneeded indirection

--- scripts/podman-entrypoint.sh
+++ scripts/podman-entrypoint.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-# export USER=file-builder
-<<<<<<< Updated upstream
-# curl https://install.meteor.com/?release=2.3.5 | sh
-=======
-# # curl https://install.meteor.com/?release=2.3.5 | sh
->>>>>>> Stashed changes
-# export PATH=$PATH:/home/file-builder/.meteor
-# export METEOR_WAREHOUSE_DIR=/home/file-builder/.meteor
 "$@"

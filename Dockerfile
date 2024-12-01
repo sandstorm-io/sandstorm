2546ff837cabdc99a36b410a5cfdd9a433f386a2
Eric Schultz <eric@wwahammy.com>
2024-12-01 16:23:37 -0600

    Add container-build tool

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM docker.io/ubuntu:20.04
+RUN apt-get -qq update && \
+    DEBIAN_FRONTEND=noninteractive apt-get -qq install -y build-essential libcap-dev xz-utils zip \
+    unzip strace curl discount git python3 zlib1g-dev \
+    cmake flex bison locales clang && \
+    rm -rf /var/lib/apt/lists/*
+COPY --from=docker.io/golang:1.21 /usr/local/go /usr/local/go
+ENV PATH "$PATH:/usr/local/go/bin"
+RUN groupadd -g 1000 file-builder
+RUN useradd -m -g 1000 -u 1000 file-builder
+RUN chown -R  file-builder:file-builder /usr/local
+USER file-builder
+RUN curl https://install.meteor.com/?release=2.3.5 | sh
+USER root
+RUN chown -R  root:root /usr/local
+USER file-builder
+ENV PATH $PATH:/home/file-builder/.meteor
+ENV METEOR_WAREHOUSE_DIR /home/file-builder/.meteor
+ENV USER file-builder
+WORKDIR /sandstorm
---
+++ container-build.py
@@ -0,0 +1,73 @@
+#!/bin/python
+import argparse, shlex, subprocess, os, getpass
+
+# from https://stackoverflow.com/a/4760274/259046
+def runProcess(exe):
+    p = subprocess.run(shlex.split(exe), stderr=subprocess.STDOUT)
+    while(True):
+        if not isinstance(p, subprocess.CompletedProcess):
+            retcode = p.poll()
+        else:
+            retcode = p.returncode
+        
+        stdout = p.stdout
+        print(stdout)
+        if retcode is not None:
+            if retcode != 0:
+                exit(retcode)
+            else:
+                break
+
+
+
+
+parser = argparse.ArgumentParser(description='Build Sandstorm using an Ubuntu 20.04 Docker/OCI container')
+parser.add_argument("action", choices=["make", "prepare", "shell"], default="make", nargs="?")
+parser.add_argument('--container-builder', dest="container_builder", default='podman', help='Command you run for building container from command line')
+parser.add_argument('--container-runner', dest="container_runner", default='podman', help='Command you run for running container from command line')
+parser.add_argument('args', nargs=argparse.REMAINDER)
+
+args = parser.parse_args()
+
+def prepare():
+    script ="{builder_cmd} build .  -t sandstorm-build".format(builder_cmd=args.container_builder)
+    print(script)
+    runProcess(script)
+
+def prepare_cmd(command):
+    return "{runner_cmd} run --rm -ti \
+        -v {pwd}:/sandstorm \
+        -v {pwd}/scripts/podman-entrypoint.sh:/podman-entrypoint.sh \
+        --userns=keep-id:uid=1000,gid=1000 \
+        --entrypoint=/podman-entrypoint.sh \
+        --cap-add=SYS_PTRACE  sandstorm-build {command} {args}".format(
+            runner_cmd=args.container_runner, 
+            pwd=os.getcwd(),
+            command=command,
+            args=' '.join(args.args)
+        )
+
+        #   
+
+def make():
+    script = prepare_cmd("make")
+    print(script)
+    runProcess(script)
+
+def shell():
+    script = prepare_cmd("bash")
+    print(script)
+    runProcess(script)
+
+prepare()
+
+if (args.action == "make"):
+    make()
+
+if (args.action == "shell"):
+    shell()
+
+
+
+
+
